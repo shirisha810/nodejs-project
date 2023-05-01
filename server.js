864ce94227d0c37f7a9e16cfa41e9cbc3d63eabf
shirisha810 <132137641+shirisha810@users.noreply.github.com>
2023-05-01 10:48:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World.... HELLO DEVOPS ENGINEERS');
+  res.send('Hello World.... HELLO DEVOPS ENGINEERS FROM SRISH');
 });
 
 app.listen(PORT, HOST, () => {
